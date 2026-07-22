552e1e931eb8a72d42f46de3266dfb9ad636d06f
minit <minit@example.com>
2026-07-22 14:14:48 +0900

    Phase4: Parentに引数を追加

--- Parent.jsx
+++ Parent.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Counter from './Counter';
 
-const Parent = () => {
+const Parent = ({ initialValue = 0 }) => {
   return (
     <div>
-      <Counter initialValue={0} />
+      <h2>Parent Component</h2>
+      <Counter initialValue={initialValue} />
     </div>
   );
 };
